122857bea37b311c7354f06c9a020076c63cb582
Martin Osterwalder <ma-osti@hotmail.com>
2015-12-28 13:40:59 +0100

    Initalize Version 1 Complete

---
+++ OstiLibrary/OstiLibrary/Helper/Configuration/ConfigWrapper.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OstiLibrary.Helper.Configuration
+{
+    public static class ConfigWrapper
+    {
+        public static string LogDirectoryName = "LogDirectory";
+        public static string LogLevelName = "LogLevel";
+        private static string AppSettingsName = "appSettings";
+        public static string[] PossibleTypes = {"string","int", "long","decimal","double","float","bool"}; 
+
+        /// <summary>
+        /// Get the Value of the generic Configsetting. PossibleTypes you see in the PossibleTypes (string[]).
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="settingName"></param>
+        /// <returns></returns>
+        public static object GetSetting<T>(string settingName)
+        {
+            switch (typeof(T).ToString().ToLower())
+            {
+                case "string":
+                    try { return ConfigurationManager.AppSettings[settingName]; } catch { return null; }
+                case "int":
+                    try { return ParseInt(ConfigurationManager.AppSettings[settingName]); } catch { return null; }
+                case "long":
+                    try { return ParseLong(ConfigurationManager.AppSettings[settingName]); } catch { return null; }
+                case "decimal":
+                    try { return ParseDecimal(ConfigurationManager.AppSettings[settingName]); } catch { return null; }
+                case "double":
+                    try { return ParseDouble(ConfigurationManager.AppSettings[settingName]); } catch { return null; }
+                case "float":
+                    try { return ParseFloat(ConfigurationManager.AppSettings[settingName]); } catch { return null; }
+                case "bool":
+                    try { return ParseBool(ConfigurationManager.AppSettings[settingName]); } catch { return null; }
+                default:
+                    return null;
+            }
+        }
+        public static bool SetSetting<T>(string settingName, T value)
+        {
+            bool success = false;
+            switch (typeof(T).ToString().ToLower())
+            {
+                case "string":
+                    return SetSetting(settingName, value);
+                case "int":
+                    return SetSetting(settingName, value.ToString());
+                case "long":
+                    return SetSetting(settingName, value.ToString());
+                case "double":
+                    return SetSetting(settingName, value.ToString());
+                case "decimal":
+                    return SetSetting(settingName, value.ToString());
+                case "float":
+                    return SetSetting(settingName, value.ToString());
+                case "bool":
+                    return SetSetting(settingName, value.ToString());
+            }
+            return success;
+        }
+
+        private static bool SetSetting(string settingName, string value)
+        {
+            bool success = true;
+            try
+            {
+                System.Configuration.ConfigurationManager.AppSettings[settingName] = value;
+                System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings[settingName].Value = value;
+                config.Save(ConfigurationSaveMode.Modified);
+
+                ConfigurationManager.RefreshSection(AppSettingsName);
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+            
+            return success;
+        }
+
+        public static string LogDirectory
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings[LogDirectoryName];
+            }
+            set
+            {
+                System.Configuration.ConfigurationManager.AppSettings[LogDirectoryName] = value;
+                System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings[LogDirectoryName].Value = value;
+                config.Save(ConfigurationSaveMode.Modified);
+
+                ConfigurationManager.RefreshSection(AppSettingsName);
+            }
+        }
+        public static int LogLevel
+        {
+            get
+            {
+                return ParseInt(ConfigurationManager.AppSettings[LogLevelName]);
+            }
+            set
+            {
+                ConfigurationManager.AppSettings[LogLevelName] = value.ToString();
+                System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                config.AppSettings.Settings[LogLevelName].Value = value.ToString();
+                config.Save(ConfigurationSaveMode.Modified);
+
+                ConfigurationManager.RefreshSection(AppSettingsName);
+            }
+        }
+
+
+
+
+        private static int ParseInt(string intValue)
+        {
+            int value = 0;
+            Int32.TryParse(intValue, out value);
+            return value;
+        }
+        private static long ParseLong(string intValue)
+        {
+            long value = 0;
+            Int64.TryParse(intValue, out value);
+            return value;
+        }
+        private static decimal ParseDecimal(string decimalValue)
+        {
+            decimal value = 0.0m;
+            Decimal.TryParse(decimalValue, out value);
+            return value;
+        }
+        private static double ParseDouble(string doubleValue)
+        {
+            double value = 0.0d;
+            Double.TryParse(doubleValue, out value);
+            return value;
+        }
+        private static float ParseFloat(string floatValue)
+        {
+            float value = 0.0f;
+            float.TryParse(floatValue, out value);
+            return value;
+        }
+        private static bool ParseBool(string boolValue)
+        {
+            bool value = false;
+            Boolean.TryParse(boolValue,out value);
+            return value;
+        }
+
+    }
+}
---
+++ OstiLibrary/OstiLibrary/Helper/Logging/Logger.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OstiLibrary.Helper.Configuration;
+
+namespace OstiLibrary.Helper.Logging
+{
+    public static class Logger
+    {
+        private static int _ALL = 3;
+        private static int _WARNING = 2;
+        private static int _ERROR = 1;
+        private static int _OFF = 0;
+        private static int logLevel = ConfigWrapper.LogLevel;
+        private static string logDirectory = ConfigWrapper.LogDirectory;
+
+        public static int LogLevel
+        {
+            get { return logLevel; }
+            set { logLevel = value; }
+        }
+        public static string LogDirectory
+        {
+            get { return logDirectory; }
+            set { logDirectory = value; }
+        }
+
+        internal static int ALL
+        {
+            get { return _ALL; }
+        }
+        internal static int ERROR
+        {
+            get { return _ERROR; }
+        }
+        internal static int INFO
+        {
+            get { return _WARNING; }
+        }
+        internal static int OFF
+        {
+            get { return _OFF; }
+        }
+
+
+        public static void Append(string message, int level)
+        {
+            try
+            {
+
+
+                if (logLevel >= level)
+                {
+                    DateTime dt = DateTime.Now;
+                    string filePath = Path.Combine(logDirectory, dt.ToString("yyyyMMdd") + ".log");
+                    if (!Directory.Exists(logDirectory))
+                    {
+                        Directory.CreateDirectory(logDirectory);
+                    }
+                    if (!File.Exists(filePath))
+                    {
+                        FileStream fs = File.Create(filePath);
+                        fs.Close();
+                    }
+                    try
+                    {
+                        StreamWriter sw = File.AppendText(filePath);
+                        sw.WriteLine(dt.ToString("hh:mm:ss") + " | " + message);
+                        sw.Flush();
+                        sw.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine("Fehler beim Logger");
+                        Debug.WriteLine(e.Message);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Fehler beim Logger");
+                Debug.WriteLine(e.Message);
+            }
+        }
+
+        public static void Append(string message, LogLevel level)
+        {
+            Append(message, (int)level);
+        }
+    }
+
+    public enum LogLevel
+    {
+        Error = 1,
+        Warning = 2,
+        Information = 3
+    }
+}
+
